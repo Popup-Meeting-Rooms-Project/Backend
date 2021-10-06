ccf39259094e451db78aea870921300a7641ffd1
MDorsaz <michel.dorsaz@students.hevs.ch>
2021-10-06 10:08:30 +0300

    removed test messages

--- js_files/mqtt/mqttClient.js
+++ js_files/mqtt/mqttClient.js
@@ -15,33 +15,6 @@ const mqttClient = mqtt.connect(
 mqttClient.on("connect", function(){	
 
     console.log("Connected to Broker");
-    
-    console.log("Sending test message");
-
-    msg1 = {
-        "id": "CA:F7:44:DE:EB:E1",
-        "room_number": 4,
-        "occupancy": true,
-        "timestamp": "2017-07-01T18:36:57.229115"
-      }
-
-    msg2 = {
-    "id": "CB:F8:45:DF:EC:E2",
-    "room_number": 5,
-    "occupancy": true,
-    "timestamp": "2017-07-01T18:36:57.229115"
-    }
-
-    msg3 = {
-        "id": "CA:F7:44:DE:EB:E1",
-        "room_number": 4,
-        "occupancy": false,
-        "timestamp": "2017-07-01T18:36:57.229115"
-      }
-
-    mqttClient.publish(mqttClientConfig.topic, `${JSON.stringify(msg1)}`)
-    mqttClient.publish(mqttClientConfig.topic, `${JSON.stringify(msg2)}`)
-    mqttClient.publish(mqttClientConfig.topic, `${JSON.stringify(msg3)}`)
 
 })
 
